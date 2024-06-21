c7a397cfafa1a3e8b1910f39b4f4e0a12054bb1a
Marjan Shahbakhsh <marjanshahbakhsh23@gmail.com>
2024-06-21 21:21:23 +0330

    feat: initialize project structure with Vue,  TypeScript, and Bootstrap v5

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/"></RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
