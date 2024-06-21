31ed0edbdf3ddba0137a5a45d8186d50ba4fca02
Marjan Shahbakhsh <99913636+mrjnsh@users.noreply.github.com>
2024-06-21 21:24:21 +0330

    feat: initialize project structure with Vue, (#1)
    TypeScript, and Bootstrap v5

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="bg-secondary">{{ msg }}</h1>
+  </div>
+</template>
+
