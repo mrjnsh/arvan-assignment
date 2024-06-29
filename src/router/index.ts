0f2eb4681eef84805254af188fa42813602ce2cf
Marjan Shahbakhsh <marjanshahbakhsh23@gmail.com>
2024-06-29 11:04:00 +0330

    fix: change landing page to login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import { useAuth } from '@/modules/useAuth'
 import RegisterView from '@/views/RegisterView.vue'
@@ -14,7 +13,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      redirect: '/login'
     },
     {
       path: '/login',
